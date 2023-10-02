28c91c69c050e7020be0506dcabc9182492a64d4
Dani Laudza <danilaudza@gmail.com>
2023-10-02 10:03:57 +0700

    [ADD] feature neww

--- app/fine/(tabs)/analisis.js
+++ app/fine/(tabs)/analisis.js
@@ -4,7 +4,7 @@ import React from 'react'
 const analisis = () => {
   return (
     <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text>Malas</Text>
+      <Text>Malasz</Text>
     </View>
   )
 }
